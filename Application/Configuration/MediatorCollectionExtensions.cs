12e93f051f39c3b8791e63d299dc9b5649e1ed2b
LucasGanciar <lucas.ganciar@charityjob.co.uk>
2024-05-17 19:26:51 +0100

    .

--- Application/Configuration/MediatorCollectionExtensions.cs
+++ Application/Configuration/MediatorCollectionExtensions.cs
@@ -1,24 +1,23 @@
-﻿using FluentValidation.Results;
-using MediatR;
+﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 using Application.Validator;
 using FluentValidation;
+using Application.Mediator.Command;
 
 namespace Application.Configuration;
 public static class MediatorCollectionExtensions
 {
     private static MediatRServiceConfiguration AddValidation<TRequest>(this MediatRServiceConfiguration cfg) where TRequest : notnull
     {
-        return cfg.AddBehavior<IPipelineBehavior<TRequest, ValidationResult>, ValidationBehavior<TRequest>>();
+        return cfg.AddBehavior<IPipelineBehavior<TRequest, bool>, ValidationBehavior<TRequest>>();
     }
 
     public static IServiceCollection AddMediator(this IServiceCollection services)
     {
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         return services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly())
-            .AddValidation<CreateContactValidator>()
-            .AddValidation<UpdateContactValidator>()
-            );
+            .AddValidation<CreateContactCommand>()
+            .AddValidation<UpdateContactCommand>());
     }
 }
--- Application/Validator/ValidationBehavior.cs
+++ Application/Validator/ValidationBehavior.cs
@@ -1,18 +1,17 @@
 ﻿using FluentValidation;
-using FluentValidation.Results;
 using MediatR;
 
 namespace Application.Validator;
-public class ValidationBehavior<TRequest>(IValidator<TRequest> validator) : IPipelineBehavior<TRequest, ValidationResult> where TRequest : notnull 
+public class ValidationBehavior<TRequest>(IValidator<TRequest> validator) : IPipelineBehavior<TRequest, bool> where TRequest : notnull 
 {
     private readonly IValidator<TRequest> validator = validator ?? throw new ArgumentNullException(nameof(validator));
 
-    public async Task<ValidationResult> Handle(TRequest request, RequestHandlerDelegate<ValidationResult> next, CancellationToken cancellationToken)
+    public async Task<bool> Handle(TRequest request, RequestHandlerDelegate<bool> next, CancellationToken cancellationToken)
     {
         var validationResult = await validator.ValidateAsync(request, cancellationToken);
         if (!validationResult.IsValid)
         {
-            return validationResult;
+            return validationResult.IsValid;
         }
 
         return await next();
